da2c9cf7eb7b50c6916fbd8d4d32d5b235eaf043
Walter Hudson <hudson.walterf@gmail.com>
2022-10-17 13:22:15 -0400

    first commit

---
+++ lesson2.cpp
@@ -0,0 +1,26 @@
+// The following files are notes taken for learning C++
+
+#include <iostream>
+
+/* 
+#include adds standard or user-defined 
+header files to a program
+*/
+/////////////////////////////////////////////
+/*
+the <iostream> header defines stream objects
+which input and ouput data
+*/
+/////////////////////////////////////////////
+
+using namespace std; /* 'namespace' is known as a
+                        'declarative region.' this means
+                        it provides a scope to elements
+                        within itself. | The 'std' namespace
+                        includes standard C++ library features.
+                    */
+int main() 
+    {
+        cout << "Hello world!";
+        return 0; 
+    }
---
+++ lesson3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl; /*
+                                    the 'endl' command
+                                    tells the compiler to
+                                    move to a new line before
+                                    executing the second 'cout'
+                                    command
+                                    */
+    cout << "I love programming! \n";
+    cout << "yo waddup";
+    return 0;
+}
